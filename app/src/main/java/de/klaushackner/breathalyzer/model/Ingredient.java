4e87491c1f57efffe749209cc36b9016844530da
DieEchteNilente <klaushackner@live.de>
2017-12-26 22:39:58 +0100

    Refactors Ingredient, Recipe

--- app/src/main/java/de/klaushackner/breathalyzer/model/Ingredient.java
+++ app/src/main/java/de/klaushackner/breathalyzer/model/Ingredient.java
@@ -3,57 +3,55 @@ package de.klaushackner.breathalyzer.model;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+/**
+ * A ingredient of a recipe, e.g. Beer or Vodka
+ */
+
 public class Ingredient {
-    private String name;
-    private double percentage;
-    private double amount;
 
-    public Ingredient(String name, double percentage, double amount) {
+    protected String name;
+    protected double alc_content;
+    protected double quantity;
+
+    /**
+     * @param name        name of the ingredient
+     * @param alc_content the alcohol content of the ingredient in Vol.-%/100 (5% -> 0.05)
+     * @param quantity    the quantity of the ingredient in *ml*
+     */
+    public Ingredient(String name, double alc_content, double quantity) {
         this.name = name;
-        this.percentage = percentage;
-        this.amount = amount;
+        this.alc_content = alc_content;
+        this.quantity = quantity;
     }
 
-    public Ingredient(JSONObject ingredientAsJSON) {
+    /**
+     * Converts a Ingredient as JSONObject back to a Ingredient-Object
+     *
+     * @param ingredient
+     */
+    public Ingredient(JSONObject ingredient) {
         try {
-            this.name = ingredientAsJSON.getString("name");
-            this.percentage = ingredientAsJSON.getDouble("percentage");
-            this.amount = ingredientAsJSON.getDouble("amount");
+            this.name = ingredient.getString("name");
+            this.alc_content = ingredient.getDouble("alc_content");
+            this.quantity = ingredient.getDouble("quantity");
         } catch (JSONException e) {
             e.printStackTrace();
         }
     }
 
-    public String getName() {
-        return name;
+    /**
+     * @return mass of the alcohol = quantity [ml] * alcohol content * density of alcohol (0.8 g/ml)
+     */
+    public double alc_mass(){
+        return quantity * alc_content * 0.8;
     }
 
-    public double getPercentage() {
-        return percentage;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public JSONObject toJSON() {
-        JSONObject j = new JSONObject();
+    public JSONObject json() {
         try {
-            j.put("name", name);
-            j.put("percentage", percentage);
-            j.put("amount", amount);
+            return new JSONObject().put("name", name).put("alc_content", alc_content).put("quantity", quantity);
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        return j;
-    }
-
-    @Override
-    public String toString() {
-        return toJSON().toString();
-    }
-
-    public boolean compareTo(Ingredient compare) {
-        return this.toString().compareTo(compare.toString()) == 0;
+        return null;
     }
 }
