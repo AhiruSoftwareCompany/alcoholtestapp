28eff8373983fc5b9c10f621cc29e8e45f092a11
DieEchteNilente <klaushackner@live.de>
2017-12-27 19:37:13 +0100

    Makes vars of Ingredient, Recipe public; fixes typos

--- app/src/main/java/de/klaushackner/breathalyzer/model/Ingredient.java
+++ app/src/main/java/de/klaushackner/breathalyzer/model/Ingredient.java
@@ -9,18 +9,18 @@ import org.json.JSONObject;
 
 public class Ingredient {
 
-    protected String name;
-    protected double alc_content;
-    protected double quantity;
+    public String name;
+    public double alcContent;
+    public double quantity;
 
     /**
-     * @param name        name of the ingredient
-     * @param alc_content the alcohol content of the ingredient in Vol.-%/100 (5% -> 0.05)
-     * @param quantity    the quantity of the ingredient in *ml*
+     * @param name       name of the ingredient
+     * @param alcContent the alcohol content of the ingredient in Vol.-%/100 (5% -> 0.05)
+     * @param quantity   the quantity of the ingredient in *ml*
      */
-    public Ingredient(String name, double alc_content, double quantity) {
+    public Ingredient(String name, double alcContent, double quantity) {
         this.name = name;
-        this.alc_content = alc_content;
+        this.alcContent = alcContent;
         this.quantity = quantity;
     }
 
@@ -32,7 +32,7 @@ public class Ingredient {
     public Ingredient(JSONObject ingredient) {
         try {
             this.name = ingredient.getString("name");
-            this.alc_content = ingredient.getDouble("alc_content");
+            this.alcContent = ingredient.getDouble("alcContent");
             this.quantity = ingredient.getDouble("quantity");
         } catch (JSONException e) {
             e.printStackTrace();
@@ -42,13 +42,13 @@ public class Ingredient {
     /**
      * @return mass of the alcohol = quantity [ml] * alcohol content * density of alcohol (0.8 g/ml)
      */
-    public double alc_mass(){
-        return quantity * alc_content * 0.8;
+    public double alc_mass() {
+        return quantity * alcContent * 0.8;
     }
 
     public JSONObject json() {
         try {
-            return new JSONObject().put("name", name).put("alc_content", alc_content).put("quantity", quantity);
+            return new JSONObject().put("name", name).put("alcContent", alcContent).put("quantity", quantity);
         } catch (JSONException e) {
             e.printStackTrace();
         }
