3725e6aed0d084d32456fb4e8c477ae8f9c9dcdb
dieechtenilente <dieechtenilente@users.noreply.github.com>
2020-10-11 19:04:05 +0200

    Layout optimization

--- app/src/main/java/de/ahirusoftware/breathalyzer/MixtureAdapter.java
+++ app/src/main/java/de/ahirusoftware/breathalyzer/MixtureAdapter.java
@@ -1,7 +1,10 @@
 package de.ahirusoftware.breathalyzer;
 
 import android.content.Context;
+
 import androidx.annotation.NonNull;
+
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,24 +31,35 @@ public class MixtureAdapter extends ArrayAdapter<Mixture> {
         View v = inflater.inflate(R.layout.items_mixture, parent, false);
         Mixture mixture = getItem(position);
 
-
-        TextView name = v.findViewById(R.id.name);
-        TextView amount = v.findViewById(R.id.amount);
-        TextView percentage = v.findViewById(R.id.percentage);
-        ImageView image = v.findViewById(R.id.image);
-
         DecimalFormat format = new DecimalFormat("#.##");
         format.setDecimalSeparatorAlwaysShown(false);
 
+        TextView name = v.findViewById(R.id.name);
         name.setText(mixture.name);
-        if (mixture.getAmount() < 100) {
-            amount.setText(format.format(mixture.getAmount()) + " ml");
-        } else {
-            amount.setText(format.format(mixture.getAmount() / 1000) + " l");
+
+        if(mixture.getAlcContent() + mixture.getAmount() == 0){
+            name.getLayoutParams().height = ViewGroup.LayoutParams.WRAP_CONTENT;
         }
 
-        percentage.setText(format.format(mixture.getAlcContent() * 100) + " %");
+        TextView amount = v.findViewById(R.id.amount);
+        if (mixture.getAlcContent() > 0) {
+            if (mixture.getAmount() < 100) {
+                amount.setText(format.format(mixture.getAmount()) + " ml");
+            } else {
+                amount.setText(format.format(mixture.getAmount() / 1000) + " l");
+            }
+        } else {
+            amount.setVisibility(View.GONE);
+        }
 
+        TextView percentage = v.findViewById(R.id.percentage);
+        if (mixture.getAlcContent() > 0) {
+            percentage.setText(format.format(mixture.getAlcContent() * 100) + " %");
+        } else {
+            amount.setVisibility(View.GONE);
+        }
+
+        ImageView image = v.findViewById(R.id.image);
         if (mixture.image != null) {
             image.setImageResource(mContext.getResources().getIdentifier(mixture.image.toString(), "mipmap",
                     mContext.getApplicationContext().getPackageName()));
